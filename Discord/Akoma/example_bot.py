4b2e18f4a8f9af5f59df1e03b17d087a28d30744
Dedie <dolente@live.com>
2024-07-17 16:00:25 -0400

    More Bot updates. Cricket blend complete.

--- Discord/Akoma/example_bot.py
+++ Discord/Akoma/example_bot.py
@@ -2,6 +2,8 @@ import settings
 import discord
 from discord.ext import commands
 
+from typing import Literal
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -14,24 +16,32 @@ async def on_ready():
         bot.user.id))
     print('------')
 
+# METHODS
+
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def whois(ctx, *, member: discord.Member):
+    whois_string = (
+        "Here's what I found about {0}:\n\n"
+        "> **Display Name:** {1}\n"
+        "> **Join Date:** {2}\n"
+        "> **Top Role:** {3}").format(
+        ctx.message.content.replace("|whois", "").strip(),
+        member.display_name,
+        member.joined_at,
+        str(member.top_role).replace("@", "@/")
+    )
 
+    await ctx.send(whois_string)
+
+# ------
+
+# TESTS
+
+@bot.command()
+async def shop(ctx, buy_sell: Literal['buy', 'sell'], amount: Literal[1, 2], *, item: str):
+    await ctx.send(f'{buy_sell.capitalize()}ing {amount} {item}(s)!')
+
+#------
+
+# run bot
 bot.run(settings.DISCORD_API_SECRET)
-
-"""
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print("We have logged in as {0}".format(client.user))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello! Katcherr")
-
-client.run(settings.DISCORD_API_SECRET)"""
